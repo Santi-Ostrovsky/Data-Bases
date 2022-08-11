e4602fa316cccc1f0640fc99ce735c9dd048d05f
Santi O <s.ostrovsky@hotmail.com>
2022-08-10 23:49:10 -0300

    update4.0

--- 10_sub-queries.sql
+++ 10_sub-queries.sql
@@ -1,12 +1,15 @@
 /*
 SUB-QUERIES
 
+Los SUB-QUERIES se utilizan para filtrar información dentro de otro filtro (como una especie de 'WHERE' anidado, cuando una query depende de otra query).       */
 
+SELECT * FROM personas WHERE id IN (
+    SELECT id FROM ciudades WHERE id > 2
+);
 
+/*
+- Primero se resuelve el query interno, devolviendo los IDs mayores a 2 en la tabla 'ciudades' (correspondiente sólo a: ID = 3, CIUDAD = New York).
 
-------------------------------------------------------------- 
+- Después se resuelve el query externo, devoviendo todos los registros en la tabla personas, en los que el ID sean los resultantes de la operación anterior, en este caso, ID = 3 (correspondiente a ID = 3 - John Wayne).
 
-
-
-********************
-*/
+------------------------------------------------------------- */
